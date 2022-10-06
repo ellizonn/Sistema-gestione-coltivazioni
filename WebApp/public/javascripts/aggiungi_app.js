aea2f5a10253d2680d51b9115a91e5289b928d11
20034670 <20034670@studenti.uniupo.it>
2022-10-06 15:21:39 +0200

    niente

--- WebApp/public/javascripts/aggiungi_app.js
+++ WebApp/public/javascripts/aggiungi_app.js
@@ -31,11 +31,70 @@ class nuova_proprieta_app{
 
       // per non farlo inviare prima che sia tutto a posto
       sessionStorage.setItem("id_azienda",info_proprieta.fk_azienda);
+//NB CAPIRE COSA MANCA PER FARE LA POST
       //console.log(info_proprieta.fk_azienda);
-     
+      
+      var z = document.createElement('div'); // is a node
+      z.innerHTML = `
+      <form encType="multipart/form-data" id="nuova_proprieta" class="row g-3" action="http://localhost:3000/v1/aziende/${info_proprieta.fk_azienda}/proprieta" method="post"> <!--Serve l'id dell'azienda qui, come ce lo metto-->
+        <div class="col-md-6">
+          <label for="inputEstensione" class="form-label">Estensione ettari</label>
+          <input type="text" class="form-control" id="estensione_ettari" placeholder="Inserire..." required>
+        </div>
+        <div class="col-md-6">
+          <label for="inputColtura" class="form-label">Coltura</label>
+          <input type="text" class="form-control" id="coltura" placeholder="Inserire..." required>
+        </div>
+        <div class="col-md-6">
+          <label for="inputData" class="form-label">Data semina</label>
+          <input type="date" class="form-control" id="data_semina" required>
+        </div>
+        <div class="col-md-6">
+          <label for="inputTipo" class="form-label">Tipo proprieta'</label>
+          <select id="tipo_proprieta" class="form-select" required>
+          <option selected>Scegli...</option>
+          <option>Campo</option>
+          <option>Serra</option>
+          <option>Vigneto</option>
+          </select>
+        </div>
+        <div class="col-md-6">
+          <label for="inputLat" class="form-label">Latitudine</label>
+          <input type="text" class="form-control" id="lat" placeholder="0° N" required>
+        </div>
+        <div class="col-md-6">
+          <label for="inputLong" class="form-label">Longitudine</label>
+          <input type="text" class="form-control" id="long" placeholder="0° N" required>
+        </div>
+          <center>
+        <div class="col-md-6">
+          <label for="inputCopertura" class="form-label">Copertura mobile</label>
+          <select id="copertura_mobile" class="form-select" required>
+          <option selected>Scegli...</option>
+          <option>True</option>
+          <option>False</option>
+          </select>
+        </div>
+          </center>
+        <div class="col-12">
+          <input class="form-check-input" type="checkbox" id="gridCheck" required>
+          <label class="form-check-label" for="gridCheck">
+          Trattamento dati proprieta'
+          </label>
+        </div>
+        <div class="col-12">
+        <button type="submit" class="btn btn-primary">Aggiungi</button>
+        </div>
+      </form>
+
+   `;
   
+      var h = document.getElementById('form-aggiungi');
+      h.appendChild(z);
+      console.log(info_proprieta.fk_azienda);
 
       //Qui bisogna aggiungere l'aggiunta dei dispositivi
+      
         }
        
     }
